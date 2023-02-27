bf7ee176f42e5fda93edba09dad47c50262bada1
OmarMWarraich <earthling986@gmail.com>
2023-02-27 20:45:33 +0500

    feat: Controller root

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,8 @@
-// Entry point for the build script in your package.json
-import "@hotwired/turbo-rails"
-import "./controllers"
+import React from "react";
+import ReactDOM from "react-dom";
+
+const App = () => {
+    return <div>Hello World!</div>;
+    };
+
+ReactDOM.render(<App />, document.getElementById("app"));
